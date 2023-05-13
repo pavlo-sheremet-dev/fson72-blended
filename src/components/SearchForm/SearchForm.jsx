c460d2a8e2a79291f167d5fd4fa004810c7b2630
Ruslan <Giovingo92@gmail.com>
2023-05-13 19:45:25 +0300

    gallery

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,31 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const query = e.target.elements.search.value.toLowerCase().trim();
+
+    if (!query) {
+      return;
+    }
+
+    this.props.onSubmit(query);
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,11 +2,26 @@ import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import axios from 'axios';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+  };
+
+  // async componentDidMount() {
+  //   ImageService.getImages(this.state.query, this.state.page);
+  // }
+
+  getQuery = query => {
+    this.setState({ query });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.getQuery} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
